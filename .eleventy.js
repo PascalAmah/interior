117b88b580a744e15284c1949428bd01e342268a
PascalAmah <pascal.amah10@gmail.com>
2026-06-20 07:50:11 +0100

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function (eleventyConfig) {
+  // Pass through static assets
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/admin");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data",
+    },
+  };
+};
